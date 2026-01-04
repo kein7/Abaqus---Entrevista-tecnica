9f3c4ffa4ba4b1afbcfa6b5f8e5d2a655f466f8f
kevin <kevin.o.nicolas.m@gmail.com>
2026-01-04 20:58:09 -0300

    fix: now rest api is called from the template. feat: dashboard chart initialition changed to match with these implementation

--- investments/views.py
+++ investments/views.py
@@ -5,13 +5,9 @@ from investments.models.models import Portfolio
 
 def portfolio_dashboard(request, pk):
     portfolio = Portfolio.objects.get(pk=pk)
-    
-    # Obtenemos el historial (Punto 4)
-    # En un caso real, start_date y end_date vendrían de request.GET
-    history_data = portfolio_history_selector(pk, '2022-02-15', '2023-02-16')
-    
+    # Ya no llamamos al selector aquí. Solo pasamos el ID.
     context = {
         'portfolio': portfolio,
-        'history_json': json.dumps(history_data), # Lo pasamos como string JSON
+        'portfolio_id': pk,
     }
     return render(request, 'dashboard.html', context)
